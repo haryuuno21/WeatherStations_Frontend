7990e57f5477d90d8be3b0d1b58a2a99766dcf3c
haryuuno21 <d.shilenok@mail.ru>
2024-11-17 15:35:24 +0300

    tauri app

--- weather_stations_app/src/App.tsx
+++ weather_stations_app/src/App.tsx
@@ -1,14 +1,37 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { StationInfoPage} from "./pages/StationInfoPage/StationInfoPage.tsx";
-import { StationsPage } from "./pages/StationsPage/StationsPage.tsx";
+import { StationInfoPage } from "./pages/StationInfoPage/StationInfoPage";
+import { StationsPage } from "./pages/StationsPage/StationsPage";
 import { HomePage } from "./pages/HomePage/HomePage";
 import { ROUTES } from "./Routes";
-import Navigation from './components/NavBar/NavBar.tsx';
+import Navigation from "./components/NavBar/NavBar";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const isTauri = Boolean((window as any).__TAURI__);
+
+    if (isTauri) {
+      const { invoke } = (window as any).__TAURI__.tauri;
+
+      // Вызов команды create
+      invoke("create")
+        .then((response: any) => console.log("Создано:", response))
+        .catch((error: any) => console.error("Ошибка создания:", error));
+
+      // Возврат функции очистки, вызов команды close
+      return () => {
+        invoke("close")
+          .then((response: any) => console.log("Закрыто:", response))
+          .catch((error: any) => console.error("Ошибка закрытия:", error));
+      };
+    } else {
+      console.warn("Приложение запущено вне среды Tauri.");
+    }
+  }, []);
+
   return (
     <BrowserRouter>
-    <Navigation/>
+      <Navigation />
       <Routes>
         <Route path={ROUTES.HOME} index element={<HomePage />} />
         <Route path={ROUTES.STATIONS} element={<StationsPage />} />
